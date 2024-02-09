d35009a29e502afa207836401eee97d8a8d9a43b
Natia Kerdzaia <silverlizard@Natias-MacBook-Air.local>
2024-02-09 14:42:19 +0400

    add transfer method

--- amocana1.py
+++ amocana1.py
@@ -4,4 +4,11 @@ class BankAccount:
         self.account_holder = account_holder
         self.balance = balance
 
+    def transfer(self, money_amount):
+        self.balance += money_amount
+        print(f"{self.account_holder}ს ანგარიშზე ({self.account_number}) ჩაირიცხა თანხა {money_amount}ლარი. \n"
+              f"ბალანსი შეადგენს: {self.balance} ლარს. \n")
 
+
+account1 = BankAccount("123456", "ნათია", 1000)
+account1.transfer(100)
