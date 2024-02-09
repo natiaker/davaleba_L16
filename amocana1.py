a5959b52d7705b67d626d27588f784fcd9e0fbf8
Natia Kerdzaia <silverlizard@Natias-MacBook-Air.local>
2024-02-09 14:51:11 +0400

    do transactions

--- amocana1.py
+++ amocana1.py
@@ -15,9 +15,15 @@ class BankAccount:
             print(f"{self.account_holder}ს ანგარიშიდან ({self.account_number}) გავიდა თანხა {money_amount}ლარი. \n"
                   f"ბალანსი შეადგენს: {self.balance} ლარს. \n")
         else:
-            print(f"{self.account_holder}ს ანგარიშზე ({self.account_number}) არ არის საკმარისი თანხა.\n")
+            print(f"{self.account_holder}ს ანგარიშზე ({self.account_number}) არ არის საკმარისი თანხა.\n"
+                  f"ნაშთი შეადგენს: {self.balance} ლარს. \n")
 
 
 account1 = BankAccount("123456", "ნათია", 1000)
 account1.transfer(100)
 account1.withdraw(20)
+
+account2 = BankAccount("789012", "ნინო", 2000)
+account2.transfer(400)
+account2.withdraw(700)
+account2.withdraw(1750)
