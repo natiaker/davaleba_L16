4504897826b3ecc69291848403b1f5e00979e926
Natia Kerdzaia <silverlizard@Natias-MacBook-Air.local>
2024-02-09 14:47:21 +0400

    add withdraw method

--- amocana1.py
+++ amocana1.py
@@ -9,6 +9,15 @@ class BankAccount:
         print(f"{self.account_holder}ს ანგარიშზე ({self.account_number}) ჩაირიცხა თანხა {money_amount}ლარი. \n"
               f"ბალანსი შეადგენს: {self.balance} ლარს. \n")
 
+    def withdraw(self, money_amount):
+        if money_amount <= self.balance:
+            self.balance -= money_amount
+            print(f"{self.account_holder}ს ანგარიშიდან ({self.account_number}) გავიდა თანხა {money_amount}ლარი. \n"
+                  f"ბალანსი შეადგენს: {self.balance} ლარს. \n")
+        else:
+            print(f"{self.account_holder}ს ანგარიშზე ({self.account_number}) არ არის საკმარისი თანხა.\n")
+
 
 account1 = BankAccount("123456", "ნათია", 1000)
 account1.transfer(100)
+account1.withdraw(20)
