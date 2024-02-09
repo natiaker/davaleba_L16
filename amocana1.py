a6afd1b4e20df1db31e8c82a1e5d383974bfcef6
Natia Kerdzaia <silverlizard@Natias-MacBook-Air.local>
2024-02-09 14:35:20 +0400

    create class BankAccount and initialize attributes

---
+++ amocana1.py
@@ -0,0 +1,7 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder, balance):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = balance
+
+
