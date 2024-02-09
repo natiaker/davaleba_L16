c324d00459af91e601db9716d5569232c48319aa
Natia Kerdzaia <silverlizard@Natias-MacBook-Air.local>
2024-02-09 15:26:05 +0400

    add show information method

--- amocana2.py
+++ amocana2.py
@@ -4,3 +4,11 @@ class Student:
         self.student_id = student_id
         self.courses = courses
 
+    def show_info(self):
+        print(f"Name: {self.name} \nStudent ID: {self.student_id} \nCourses:")
+        for course in self.courses:
+            print("     -" + course)
+
+
+student1 = Student("Nika", "001", ["Math", "Science"])
+student1.show_info()
