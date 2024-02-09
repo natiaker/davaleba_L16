4a035916d0a50faa2d2f6a2f17d1418a5824af02
Natia Kerdzaia <silverlizard@Natias-MacBook-Air.local>
2024-02-09 15:41:16 +0400

    add a course registration method and register some students

--- amocana2.py
+++ amocana2.py
@@ -8,7 +8,24 @@ class Student:
         print(f"Name: {self.name} \nStudent ID: {self.student_id} \nCourses:")
         for course in self.courses:
             print("     -" + course)
+        print()
+
+    def add_course(self, course):
+        if course not in self.courses:
+            self.courses.append(course)
+            print(f"{self.name}'s courses:")
+            for course in self.courses:
+                print("     -" + course)
+            print()
+        else:
+            print(f"{self.name} is already registered for {course} course\n")
 
 
-student1 = Student("Nika", "001", ["Math", "Science"])
+student1 = Student("Nika", "001", ["Math", "Biology", "Literature"])
 student1.show_info()
+student1.add_course("Art")
+
+student2 = Student("Tako", "002", ["Chemistry", "Physics", "Art"])
+student2.show_info()
+student2.add_course("Art")
+student2.add_course("English")
